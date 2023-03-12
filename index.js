6044a928203aa8cff299ab05defa32289c1a4501
leosilvapb <leo-leonardo-silva@hotmail.com>
2023-03-12 02:31:20 -0300

    nova rota adicionada

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.status(200).send({ message: 'Olá mundo, esse é meu novo repositório clonado' });
 })
 
+app.get('/', (req, res) => {
+    res.status(200).send({ message: 'Olá mundo' });
+})
+
 app.listen(4001, () => {
     console.log('Api rodando na porta 4001');
 })
